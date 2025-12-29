23ea3da1bc9b299e057cb1239ad0ae9985d18cfd
sujal <sujalkarki1904@gmail.com>
2025-12-30 00:14:19 +0545

    resume updated today3

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -1,39 +1,36 @@
+import { Link } from "react-router-dom";
 const Hero = () => {
   return (
     <section className="bg-black text-white flex flex-col items-start pt-26 pb-26 relative">
       <div className="container mx-auto px-6 flex flex-col md:flex-row items-center md:justify-between">
 
         {/* Left */}
-<div className="md:w-1/2 mb-10 md:mb-0">
-  <h1 className="text-5xl md:text-7xl font-extrabold">
-    <span className="bg-grey-800 text-white px-1 mr-2 ">Sujal</span>
-    <span className="bg-grey-400 text-white px-1">Karki</span>
-  </h1>
+        <div className="md:w-1/2 mb-10 md:mb-0">
+          <h1 className="text-5xl md:text-7xl font-extrabold">
+            <span className="bg-grey-800 text-white px-1 mr-2 ">Sujal</span>
+            <span className="bg-grey-400 text-white px-1">Karki</span>
+          </h1>
 
-  <p className="text-lg md:text-xl text-gray-400 mt-4">
-    I am a web developer and a student at IOEPC. I enjoy building modern web applications and am eager to learn Machine Learning and AI.
-  </p>
+          <p className="text-lg md:text-xl text-gray-400 mt-4">
+            I am a web developer and a student at IOEPC. I enjoy building modern web applications and am eager to learn Machine Learning and AI.
+          </p>
 
-  {/* Buttons */}
-  <div className="mt-6 flex gap-4">
-    <button
-      onClick={() => {
-        const section = document.getElementById("projects");
-        section?.scrollIntoView({ behavior: "smooth" });
-      }}
-      className="px-6 py-2 bg-yellow-400 text-black font-semibold rounded hover:bg-yellow-300 transition"
-    >
-      View Projects
-    </button>
+          {/* Buttons */}
+          <div className="mt-6 flex gap-4">
+            <button
+              onClick={() => {
+                const section = document.getElementById("projects");
+                section?.scrollIntoView({ behavior: "smooth" });
+              }}
+              className="px-6 py-2 bg-yellow-400 text-black font-semibold rounded hover:bg-yellow-300 transition"
+            >
+              View Projects
+            </button>
 
-    <a
-      href="/resume"
-      className="px-6 py-2 border border-yellow-400 text-yellow-400 rounded hover:bg-yellow-400 hover:text-black transition"
-    >
-      Resume
-    </a>
-  </div>
-</div>
+            <Link to="/resume" className="px-6 py-2 border border-yellow-400 text-yellow-400 rounded hover:bg-yellow-400 hover:text-black transition">Resume</Link>
+          </div>
+
+        </div>
 
 
         {/* Right */}
